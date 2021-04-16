eb2871124b8c8d0ffceba273ed9b612c6c383af6
이규민 <73277618+likuimin04@users.noreply.github.com>
2021-04-16 20:53:17 +0900

    JS: navbar toggle / project UI tweak

--- main.js
+++ main.js
@@ -12,15 +12,21 @@ document.addEventListener('scroll', () => {
 });
 
 // Navbar에 Button을 클릭했을때 원하는 위치로 이동
-const navabrMenu=document.querySelector('.navbar__menu');
-navabrMenu.addEventListener('click', (event) => {
+const navbarMenu=document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
     if(link == null) {
         return;
     }
-
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// Boggle btn 설정
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
 
 // Home 안에 있는 "contact me" 버튼 클릭 시 이벤트 발생
