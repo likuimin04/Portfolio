7fb9e210428fa82f4745198ce4f2c129b9e909d6
이규민 <73277618+likuimin04@users.noreply.github.com>
2021-04-16 18:00:17 +0900

    JS: NavarMenu

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Make navabr transparent when it is on the top
+// Navabr 스크롤했을때 Navabr에 background-color 생성
 const navbar =document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -11,4 +11,18 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+//Navbar에 Button을 클릭했을때 원하는 위치로 이동
+const navabrMenu=document.querySelector('.navbar__menu');
+navabrMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
 });
