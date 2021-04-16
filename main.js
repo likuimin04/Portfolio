3188f73ab93ef4a9f9131cfd4fd7526609674800
이규민 <73277618+likuimin04@users.noreply.github.com>
2021-04-16 18:24:48 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 const navbar =document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
@@ -13,7 +11,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
-//Navbar에 Button을 클릭했을때 원하는 위치로 이동
+// Navbar에 Button을 클릭했을때 원하는 위치로 이동
 const navabrMenu=document.querySelector('.navbar__menu');
 navabrMenu.addEventListener('click', (event) => {
     const target = event.target;
@@ -25,13 +23,26 @@ navabrMenu.addEventListener('click', (event) => {
     scrollIntoView(link);
 });
 
-//Home 안에 있는 "contact me" 버튼 클릭 시 이벤트 발생
+// Home 안에 있는 "contact me" 버튼 클릭 시 이벤트 발생
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+// Home을 스크롤 할때 점점 투명화 시키기 작업
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+
