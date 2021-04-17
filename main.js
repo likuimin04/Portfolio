c574c5a93b22e2a9a5664963a3d4242dacb096d7
이규민 <73277618+likuimin04@users.noreply.github.com>
2021-04-17 12:37:39 +0900

    JS: navbar scrolling

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ navbarMenu.addEventListener('click', (event) => {
     }
     navbarMenu.classList.remove('open');
     scrollIntoView(link);
+    selectNavItem(target);
 });
 
 // Boggle btn 설정
@@ -124,4 +125,53 @@ function scrollIntoView(selector) {
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
 
+// navbar 스크롤링 자동 선택 설정
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#project',
+    '#academy',
+    '#contact'
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`)
+);
 
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+}
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            //스크롤링이 아래로 되어서 페이지가 올라오게 설정
+            if(entry.boundingClientRect.y < 0){
+                selectedNavIndex = index + 1;
+            } else {
+                selectedNavIndex = index -1;
+            }
+        }
+    });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if(window.scrollY === 0) {
+        selectedNavIndex = 0;
+    } else if(window.scrollY + window.innerHeight === document.body.clientHeight){
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+});
